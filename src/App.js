ab81cd834f8bdac6487b825cd3d0a56bd54b0c48
gazjosef <39395712+gazjosef@users.noreply.github.com>
2019-07-28 22:52:29 +1000

    updated portfolio website

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Header from "./Header/Header";
 import Home from "./Home/Home";
 
 import "./scss/main.scss";
 
-function App() {
-  return (
-    <div id="bg-img" className="App">
-      {/* <div className="overlay">Hello</div> */}
-      <Header />
-      <Home />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMenu: false
+    };
+  }
+  toggleMenu = e => {
+    console.log("clicked");
+    // this.setState(state => ({
+    //   showMenu: true
+    // }));
+  };
+
+  render() {
+    return (
+      <div id="bg-img" className="App">
+        <div
+          className="overlay"
+          // style={{ backgroundColor: "rgba(68, 68, 68, 0.5)" }}
+        >
+          <Header toggleMenu={this.toggleMenu} />
+          <Home />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,9 +1,39 @@
 import React from "react";
 
-function Header() {
+// Select DOM Items
+// const menuBtn = document.querySelector(".menu-btn");
+// const menu = document.querySelector(".menu");
+// const menuNav = document.querySelector(".menu-nav");
+// const menuBranding = document.querySelector(".menu-branding");
+// const navItems = document.querySelectorAll(".nav-item");
+
+const Header = props => {
+  // menuBtn.addEventListener("click", toggleMenu());
+  // toggleMenu = e => {
+  //   console.log("clicked");
+  // if (!this.state.showMenu) {
+  //   menuBtn.classList.add("close");
+  //   menu.classList.add("show");
+  //   menuNav.classList.add("show");
+  //   menuBranding.classList.add("show");
+  //   navItems.forEach(item => item.classList.add("show"));
+  //   this.setState({
+  //     showMenu: true
+  //   });
+  // } else {
+  //   menuBtn.classList.remove("close");
+  //   menu.classList.remove("show");
+  //   menuNav.classList.remove("show");
+  //   menuBranding.classList.remove("show");
+  //   navItems.forEach(item => item.classList.add("show"));
+  //   this.setState({
+  //     showMenu: false
+  //   });
+  // }
+  console.log("header.js render called", props);
   return (
     <header>
-      <div className="menu-btn">
+      <div className="menu-btn" onClick={props.toggleMenu}>
         <div className="btn-line" />
         <div className="btn-line" />
         <div className="btn-line" />
@@ -38,6 +68,6 @@ function Header() {
       </nav>
     </header>
   );
-}
+};
 
 export default Header;
