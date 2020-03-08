4e221d027b29bf00e0920aa8656e4f2d52e4fecd
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-03-08 16:28:15 +1100

    Updates

---
+++ precache-manifest.07654319e6403922accebed477c13634.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5bd52749de8eb72e49e761f36370ad",
+    "url": "/modern_portfolio_react/index.html"
+  },
+  {
+    "revision": "0058e8bb2c2618d5508f",
+    "url": "/modern_portfolio_react/static/css/main.3c26584f.chunk.css"
+  },
+  {
+    "revision": "0042249dbf6608ac86f3",
+    "url": "/modern_portfolio_react/static/js/2.ba9c762d.chunk.js"
+  },
+  {
+    "revision": "dbe5ca7a82c728776bb536c3d2d64135",
+    "url": "/modern_portfolio_react/static/js/2.ba9c762d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0058e8bb2c2618d5508f",
+    "url": "/modern_portfolio_react/static/js/main.9ddadb4e.chunk.js"
+  },
+  {
+    "revision": "ce0010f39bf41e75a23e",
+    "url": "/modern_portfolio_react/static/js/runtime-main.3ccacd64.js"
+  },
+  {
+    "revision": "00051dbb5b9c7a7f4dfdf04f1b633445",
+    "url": "/modern_portfolio_react/static/media/background.00051dbb.jpg"
+  },
+  {
+    "revision": "ef93b85f91bed41bc96b6a7b7369126c",
+    "url": "/modern_portfolio_react/static/media/portrait.ef93b85f.jpg"
+  },
+  {
+    "revision": "31ad0b4d88c6a8f6d6812046509cfd67",
+    "url": "/modern_portfolio_react/static/media/portrait_small.31ad0b4d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/modern-portfolio-react/precache-manifest.e83aec1b71b1eaf3034fd5a5a69c0943.js"
+  "/modern_portfolio_react/precache-manifest.07654319e6403922accebed477c13634.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/modern-portfolio-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/modern_portfolio_react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
