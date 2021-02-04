e26b1c635fe6f660053349e4617d63524cd88000
Gareth Hind <garethjhind@gmail.com>
2021-02-04 22:40:28 +1100

    Updated react

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -3,7 +3,11 @@ import React from 'react'
 export const Home = () => {
     return (
         <section className="section-home">
-            Home
+            <div class="u-center-text u-margin-bottom-big">
+                <h2 class="heading-secondary">
+                    Gareth Hind | Web Developer
+                </h2>
+            </div>
         </section>
     )
 }
