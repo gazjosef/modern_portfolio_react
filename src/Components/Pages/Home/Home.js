333c9a68fb11a24277d28551581bd003bef32d9b
Gareth Hind <garethjhind@gmail.com>
2021-02-05 07:55:01 +1100

    Updated portfolio

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -5,31 +5,19 @@ export const Home = () => {
         <section className="section-about">
             <div className="u-center-text u-margin-bottom-big">
                 <h2 className="heading-secondary">
-                    Exciting tours for adventurous people
+                    Gareth Hind | Web Designer, Developer & Programmer
                 </h2>
             </div>
             <div className="row">
                 <div className="col-1-of-2">
-                    <h3 className="heading-tertiary u-margin-bottom-small">You're going to fall in love with nature</h3>
+                    <h3 className="heading-tertiary u-margin-bottom-small">Let me tell you a few things...</h3>
                     <p className="paragraph">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non officia quam quod facere commodi
-                        minima quisquam. Nihil at quibusdam doloribus. Animi inventore provident exercitationem cumque
-                        beatae atque quis reiciendis aliquam?
+                    Hi, my name is Gareth. I'm a Sydney-based Web Developer who enjoys building everything from small business websites to rich interactive web applications.
                     </p>
-                    <h3 className="heading-tertiary u-margin-bottom-small">Live adventures like you never have before</h3>
-                    <p className="paragraph">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non officia quam quod facere commodi
-                        minima quisquam.
-                    </p>
-                    {/* <a href="#" className="btn-text">Learn more &rarr;</a> */}
                 </div>
                 
                 <div className="col-1-of-2">
-                    {/* <div className="composition">
-                        <img src="img/nat-1-large.jpg" alt="Photo 1" className="composition__photo composition__photo--p1">
-                        <img src="img/nat-2-large.jpg" alt="Photo 2" className="composition__photo composition__photo--p2">
-                        <img src="img/nat-3-large.jpg" alt="Photo 3" className="composition__photo composition__photo--p3">
-                    </div> */}
+                    Image here
                 </div>
             </div>
 
