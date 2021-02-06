d65752cb125272fd03ea8d93c8e026dbb0ee5939
Gareth Hind <garethjhind@gmail.com>
2021-02-06 23:34:21 +1100

    Updated portfolio

--- src/Components/Pages/Work/Work.js
+++ src/Components/Pages/Work/Work.js
@@ -2,8 +2,73 @@ import React from 'react'
 
 export const Work = () => {
     return (
-        <section className="section-work">
-            Work
+        <section classNameName="section-work">
+            <div className="u-center-text u-margin-bottom-big">
+                <h2 className="heading-secondary">
+                    My Work
+                </h2>
+            </div>
+            <div className="row">
+                <h3 className="heading-tertiary u-margin-bottom-small">Check out some of my projects...</h3>
+                <div className="projects">
+                    <div className="item">
+                    <a href="https://gazjosef.github.io/blackjack-game/">
+                        <img src="img/projects/blackJack.jpg" alt="Project" />
+                    </a>
+                    <a
+                        href="https://gazjosef.github.io/blackjack-game/"
+                        className="btn-light"
+                    >
+                        <i className="fas fa-eye"></i> BlackJack Project
+                    </a>
+                    <a href="https://github.com/gazjosef/blackjack-game" className="btn-dark">
+                        <i className="fab fa-github"></i> Github
+                    </a>
+                    </div>
+
+                    <div className="item">
+                    <a href="https://gazjosef.github.io/tipping-app/">
+                        <img src="img/projects/tippingApp.jpg" alt="Project" />
+                    </a>
+                    <a href="https://gazjosef.github.io/tipping-app/" className="btn-light">
+                        <i className="fas fa-eye"></i> Tipping App Project
+                    </a>
+                    <a href="https://github.com/gazjosef/tipping-app" className="btn-dark">
+                        <i className="fab fa-github"></i> Github
+                    </a>
+                    </div>
+
+                    <div className="item">
+                    <a href="https://gazjosef.github.io/weather-app/">
+                        <img src="img/projects/weather-ts.jpg" alt="Project" />
+                    </a>
+                    <a
+                        href="https://gazjosef.github.io/weather-app/"
+                        className="btn-light"
+                    >
+                        <i className="fas fa-eye"></i> Odds Tracker
+                    </a>
+                    <a href="https://github.com/gazjosef/weather-app" className="btn-dark">
+                        <i className="fab fa-github"></i> Github
+                    </a>
+                    </div>
+
+                    <div className="item">
+                    <a href="https://gazjosef.github.io/weather-app/">
+                        <img src="img/projects/weather-ts.jpg" alt="Project" />
+                    </a>
+                    <a
+                        href="https://gazjosef.github.io/weather-app/"
+                        className="btn-light"
+                    >
+                        <i className="fas fa-eye"></i> Weather App Project
+                    </a>
+                    <a href="https://github.com/gazjosef/weather-app" className="btn-dark">
+                        <i className="fab fa-github"></i> Github
+                    </a>
+                    </div>
+                </div>
+            </div>        
         </section>
     )
 }
