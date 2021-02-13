9536ee5ad8745384593163f797f61e72854ab889
Gareth Hind <garethjhind@gmail.com>
2021-02-13 21:00:01 +1100

    Updated home

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -3,28 +3,31 @@ import React from 'react'
 export const Home = () => {
     return (
         <section className="section-about">
-            <div className="u-center-text u-margin-bottom-big">
-                <span className="heading-primary--main">
-                    Gareth Hind
-                </span>
-                <span className="heading-primary--sub">
-                    Web Designer, Developer & Programmer
-                </span>
-            </div>
             <div className="row">
-                <div className="about">
-                    <figure className="about__shape">
+                <div className="col-1-of-2">
+                    <div className="u-center-text u-margin-bottom-big">
+                        <span className="heading-primary--main">
+                            Gareth Hind
+                        </span>
+                        <span className="heading-primary--sub">
+                            Web Designer, Developer & Programmer
+                        </span>
+                    </div>
+                </div>
+                <div className="col-1-of-2">
+                    <div className="about">
+                        <figure className="about__shape">
 
-                    </figure>
-                    <div className="about__text">
-                        <h3 className="heading-tertiary u-margin-bottom-small">Let me tell you a few things...</h3>
-                        <p className="paragraph">
-                        Hi, my name is Gareth. I'm a Sydney-based Web Developer who enjoys building everything from small business websites to rich interactive web applications.
-                        </p>
+                        </figure>
+                        <div className="about__text">
+                            <h3 className="heading-tertiary u-margin-bottom-small">Let me tell you a few things...</h3>
+                            <p className="paragraph">
+                            Hi, my name is Gareth. I'm a Sydney-based Web Developer who enjoys building everything from small business websites to rich interactive web applications.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-
         </section>
     )
 }
