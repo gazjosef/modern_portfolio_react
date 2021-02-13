70198077e872bde3b9f32157e9a4fe58f6d6f54f
Gareth Hind <garethjhind@gmail.com>
2021-02-13 14:39:40 +1100

    Updated contact

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -9,15 +9,16 @@ export const Home = () => {
                 </h2>
             </div>
             <div className="row">
-                <div className="col-1-of-2">
-                    <h3 className="heading-tertiary u-margin-bottom-small">Let me tell you a few things...</h3>
-                    <p className="paragraph">
-                    Hi, my name is Gareth. I'm a Sydney-based Web Developer who enjoys building everything from small business websites to rich interactive web applications.
-                    </p>
-                </div>
-                
-                <div className="col-1-of-2">
-                    Image here
+                <div className="about">
+                    <figure className="about__shape">
+
+                    </figure>
+                    <div className="about__text">
+                        <h3 className="heading-tertiary u-margin-bottom-small">Let me tell you a few things...</h3>
+                        <p className="paragraph">
+                        Hi, my name is Gareth. I'm a Sydney-based Web Developer who enjoys building everything from small business websites to rich interactive web applications.
+                        </p>
+                    </div>
                 </div>
             </div>
 
