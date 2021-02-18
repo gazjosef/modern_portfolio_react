69b199243b405b1e4f259ed6fbc0c553d67d7f87
Gareth Hind <garethjhind@gmail.com>
2021-02-18 22:15:17 +1100

    Updated portfolio

--- src/Components/Pages/Work/Work.js
+++ src/Components/Pages/Work/Work.js
@@ -18,34 +18,65 @@ export const Work = () => {
                     <a target="_blank" href="img_5terre.jpg">
                         <img src={require("../../../Assets/img/img_lights.jpg").default} alt="Cinque Terre" width="600" height="400" />
                     </a>
-                    <div className="desc">Add a description of the image here</div>
+                    <div className="desc">
+                        <a href="https://github.com/gazjosef/blackjack-game" className="btn-dark">
+                            <i className="fab fa-github"></i> Github
+                        </a>
                     </div>
+                </div>
 
-                    <div className="gallery">
+                <div className="gallery">
                     <a target="_blank" href="img_forest.jpg">
                         <img src={require("../../../Assets/img/img_mountains.jpg").default} alt="Forest" width="600" height="400" />
                     </a>
-                    <div className="desc">Add a description of the image here</div>
+                    <div className="desc">
+                        <a href="https://github.com/gazjosef/blackjack-game" className="btn-dark">
+                            <i className="fab fa-github"></i> Github
+                        </a>
                     </div>
+                </div>
 
-                    <div className="gallery">
+                <div className="gallery">
                     <a target="_blank" href="img_lights.jpg">
                         <img src={require("../../../Assets/img/img_nature.jpg").default} alt="Northern Lights" width="600" height="400" />
                     </a>
-                    <div className="desc">Add a description of the image here</div>
+                <div className="desc">
+                    <a href="https://github.com/gazjosef/blackjack-game" className="btn-dark">
+                        <i className="fab fa-github"></i> Github
+                    </a>
                     </div>
+                </div>
 
-                    <div className="gallery">
+                <div className="gallery">
                     <a target="_blank" href="img_mountains.jpg">
                         <img src={require("../../../Assets/img/img_snow.jpg").default} alt="Mountains" width="600" height="400" />
                     </a>
-                    <div className="desc">Add a description of the image here</div>
+                    <div className="desc">
+                        <a href="https://github.com/gazjosef/blackjack-game" className="btn-dark">
+                            <i className="fab fa-github"></i> Github
+                        </a>
                     </div>
-                <div className="projects">
+                </div>
+
+                <div className="gallery">
+                    <a target="_blank" href="img_mountains.jpg">
+                        <img src={require("../../../Assets/img/img_snow.jpg").default} alt="Mountains" width="600" height="400" />
+                    </a>
+                    <div className="desc">
+                        <a href="https://github.com/gazjosef/blackjack-game" className="btn-dark">
+                            <i className="fab fa-github"></i> Github
+                        </a>
+                    </div>
+                </div>
+
+                    
+                    
+                 
+                {/* <div className="projects">
                     <div className="item">
-                        {/* <a href="https://gazjosef.github.io/blackjack-game/">
+                        <a href="https://gazjosef.github.io/blackjack-game/">
                             <img src="img/projects/blackJack.jpg" alt="Project" />
-                        </a> */}
+                        </a>
                         <a
                             href="https://gazjosef.github.io/blackjack-game/"
                             className="btn-light"
@@ -58,9 +89,9 @@ export const Work = () => {
                     </div>
 
                     <div className="item">
-                        {/* <a href="https://gazjosef.github.io/weather-app/">
+                        <a href="https://gazjosef.github.io/weather-app/">
                             <img src="img/projects/weather-ts.jpg" alt="Project" />
-                        </a> */}
+                        </a>
                         <a
                             href="https://gazjosef.github.io/weather-app/"
                             className="btn-light"
@@ -73,9 +104,9 @@ export const Work = () => {
                     </div>
 
                     <div className="item">
-                        {/* <a href="https://gazjosef.github.io/weather-app/">
+                        <a href="https://gazjosef.github.io/weather-app/">
                             <img src="img/projects/weather-ts.jpg" alt="Project" />
-                        </a> */}
+                        </a>
                         <a
                             href="https://gazjosef.github.io/weather-app/"
                             className="btn-light"
@@ -88,21 +119,21 @@ export const Work = () => {
                     </div>
 
                     <div className="item">
-                        {/* <a href="https://gazjosef.github.io/tipping-app/">
+                        <a href="https://gazjosef.github.io/tipping-app/">
                             <img src="img/projects/tippingApp.jpg" alt="Project" />
-                        </a> */}
+                        </a>
                         <a href="https://gazjosef.github.io/tipping-app/" className="btn-light">
                             <i className="fas fa-eye"></i> Tipping App
                         </a>
                         <a href="https://github.com/gazjosef/tipping-app" className="btn-dark">
                             <i className="fab fa-github"></i> Github
                         </a>
-                    </div>                    
+                    </div>
 
                     <div className="item">
-                        {/* <a href="https://gazjosef.github.io/weather-app/">
+                        <a href="https://gazjosef.github.io/weather-app/">
                             <img src="img/projects/weather-ts.jpg" alt="Project" />
-                        </a> */}
+                        </a>
                         <a
                             href="https://gazjosef.github.io/weather-app/"
                             className="btn-light"
@@ -113,7 +144,7 @@ export const Work = () => {
                             <i className="fab fa-github"></i> Github
                         </a>
                     </div>
-                </div>
+                </div> */}
             </div>        
         </section>
     )
